f34ccc4d383c09d771b2cc7d1821f3e12dae2c7a
Joshua Westgard <jwestgard@mac.com>
2021-08-16 09:53:18 -0400

    display LK entries by work

--- bootstrap/__main__.py
+++ bootstrap/__main__.py
@@ -35,16 +35,56 @@ if __name__ == "__main__":
         Column('country_id', ForeignKey('countries.id'), nullable=False)
     )
 
+    works_table = Table(
+        "works",
+        metadata,
+        Column('id', Integer, primary_key=True),
+        Column('abbreviation', String(10)),
+        Column('abbreviation_long', String(30)),
+        Column('title_lat', String),
+        Column('title_eng', String),
+        Column('refs', String)
+    )
+
+    entries_table = Table(
+        "entries",
+        metadata,
+        Column('id', Integer, primary_key=True),
+        Column('manuscript_id', Integer),
+        Column('work_id', ForeignKey('works.id'), nullable=False),
+        Column('section', String(75)),
+        Column('subsection', String(75)),
+        Column('extract', String(75)),
+        Column('page', Integer),
+        Column('settlement', String(50)),
+        Column('repository', String(75)),
+        Column('shelfmark', String(50)),
+        Column('locus', String(75)),
+        Column('provenance', String(50)),
+        Column('dating', String(10)),
+        Column('refs', String),
+        Column('rawtext', String),
+        Column('notes', String),
+    )
+
     metadata.create_all(engine)
 
-    countries_data = [row for row in csv.DictReader(open('csv/countries.csv'))]
-    cities_data = [row for row in csv.DictReader(open('csv/cities.csv'))]
-
     with engine.connect() as conn:
-        result = conn.execute(insert(countries_table), countries_data)
+        countries_data = [row for row in csv.DictReader(open('csv/countries.csv'))]
+        conn.execute(insert(countries_table), countries_data)
         conn.commit()
 
     with engine.connect() as conn:
-        result = conn.execute(insert(cities_table), cities_data)
+        cities_data = [row for row in csv.DictReader(open('csv/cities.csv'))]
+        conn.execute(insert(cities_table), cities_data)
         conn.commit()
 
+    with engine.connect() as conn:
+        works_data = [row for row in csv.DictReader(open('csv/opera.csv'))]
+        conn.execute(insert(works_table), works_data)
+        conn.commit()
+
+    with engine.connect() as conn:
+        entries_data = [row for row in csv.DictReader(open('csv/entries.csv'))]
+        conn.execute(insert(entries_table), entries_data)
+        conn.commit()
--- viewer/__main__.py
+++ viewer/__main__.py
@@ -1,10 +1,81 @@
+import csv
+import sys
+
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy import select
+from sqlalchemy import Table
+
+
+DBFILE = sys.argv[1]
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+engine = create_engine(f"sqlite+pysqlite:///{DBFILE}", echo=True, future=True)
+metadata = MetaData()
+
+works = Table('works', metadata, autoload_with=engine)
+countries = Table('countries', metadata, autoload_with=engine)
+cities = Table('cities', metadata, autoload_with=engine)
+entries = Table('entries', metadata, autoload_with=engine)
+
+
+class Work():
+    def __init__(self, **kwargs):
+        super().__init__()
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
 
 @app.route("/")
 def index():
-    return f"Hello World!")
+    with open("csv/opera.csv") as handle:
+        works = [Work(**row) for row in csv.DictReader(handle)]
+    return render_template("home.html", works=works)
+
+
+@app.route("/works")
+def works_list():
+    with engine.connect() as conn:
+        results = conn.execute(
+            select(works)
+            ).fetchall()
+    return render_template("home.html", works=results)
+
+
+@app.route("/works/<abbreviation>")
+def single_work(abbreviation):
+    with engine.connect() as conn:
+        work = conn.execute(
+            select(works).where(works.c.abbreviation == abbreviation)
+            ).fetchone()
+        mss = conn.execute(
+            select(entries).where(entries.c.work_id == work.id)
+            ).fetchall()
+    return render_template("work.html", work=work, mss=mss)
+
+
+@app.route("/countries")
+def country_list():
+    with engine.connect() as conn:
+        results = conn.execute(
+            select(text('* from countries'))
+            ).fetchall()
+    return render_template("countries.html", countries=results)
+
+
+@app.route("/countries/<country_id>")
+def city_list(country_id):
+    with engine.connect() as conn:
+        results = conn.execute(
+            select(cities).where(cities.c.country_id == country_id)
+            ).fetchall()
+    return render_template("cities.html", cities=results)
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
